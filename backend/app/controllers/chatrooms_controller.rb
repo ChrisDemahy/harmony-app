fc819030f956fa4c5de35ac5a7867c5c40f19a27
ChrisDemahy <demahyc@gmail.com>
2020-12-22 15:56:24 -0600

    🧪 Finished Initial Controller Tests

--- backend/app/controllers/chatrooms_controller.rb
+++ backend/app/controllers/chatrooms_controller.rb
@@ -1,2 +1,6 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.all
+    render json: { message: 'Ok', chatrooms: @chatrooms }, status: :ok
+  end
 end
--- backend/test/controllers/chatrooms_controller_test.rb
+++ backend/test/controllers/chatrooms_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ChatroomsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index of all chatrooms' do
+    get chatrooms_url
+    assert_response :success
+    assert_equal 'index', @controller.action_name
+
+    assert_match 'chatrooms', @response.body
+    assert_match Chatroom.all.to_json, @response.body
+  end
+
+  test 'should get attributes of a single chatroom' do
+    @chatroom = Chatroom.all.sample
+    get chatrooms_url
+    assert_response :success
+    assert_equal 'index', @controller.action_name
+
+    assert_match 'chatrooms', @response.body
+    assert_match Chatroom.all.to_json, @response.body
+  end
 end
