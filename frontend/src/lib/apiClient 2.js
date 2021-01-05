5b887880134e7391bffcacf09611b893564166fd
ChrisDemahy <demahyc@gmail.com>
2021-01-05 09:45:38 -0600

    ✅ Fixed bug where icloud was duplicating files

--- frontend/src/lib/apiClient 2.js
+++
@@ -1,9 +0,0 @@
-const URL = "http://localhost:3000";
-
-// Thunk function to fetch all chatrooms
-async function fetchChatrooms(dispatch, getState) {
-    const response = await fetch(URL + "/chatrooms");
-    const data = await response.json();
-    dispatch({ type: "SET_CHATROOMS", chatrooms: data });
-}
-export { fetchChatrooms };
