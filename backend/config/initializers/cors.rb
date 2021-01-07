467dd385ba4f519693aec4395dbfbadaf917dcb3
ChrisDemahy <demahyc@gmail.com>
2021-01-07 15:27:02 -0600

    Debugging

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'example.com' // Before
-    origins '*'
+    origins ['http://localhost:3001', 'https://app.harmony.chrisd.xyz']
 
     resource '*',
       headers: :any,
